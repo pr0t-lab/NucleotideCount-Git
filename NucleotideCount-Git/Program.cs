816228441aacc9cfaf33558d8a52b2abd017c77f
pr0t <prot@prot.com.pl>
2018-06-04 01:33:41 +0200

    Class implemented
    Ready to merge

--- NucleotideCount-Git/Program.cs
+++ NucleotideCount-Git/Program.cs
@@ -10,23 +10,40 @@ namespace NucleotideCount_Git
     {
         static void Main(string[] args)
         {
+            NucleotideCount nc = new NucleotideCount("G");
+            Console.WriteLine(nc.NucleotideCounts.Count);
+
+            Console.ReadLine();
         } // end of Main method
+
 	} // end of Program class
 	
-public class NucleotideCount
-{
-    public NucleotideCount(string sequence)
+    public class NucleotideCount
     {
-    }
+        char[] array = { 'A', 'C', 'G', 'T' }; 
+        string _sequence;
+        Dictionary<char, int> _nucleotides;
 
-    public IDictionary<char, int> NucleotideCounts
-    {
-        get
+        public NucleotideCount(string sequence)
         {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
-} // end of NucleotideCount class
+            if (!string.IsNullOrEmpty(sequence) && !sequence.All(n => n == array[0] || n == array[1] || n == array[2] || n == array[3]))
+                throw new InvalidNucleotideException();
 
-public class InvalidNucleotideException : Exception { }	
+            _sequence = sequence.ToUpper();
+        }
+
+        public IDictionary<char, int> NucleotideCounts
+        {
+            get
+            {
+                _nucleotides = new Dictionary<char, int>();
+                foreach(char n in array)
+                    _nucleotides.Add(n, _sequence.Where(c => c == n).Count());
+
+                return _nucleotides;
+            }
+        }
+    } // end of NucleotideCount class
+
+    public class InvalidNucleotideException : Exception { }	
 } // end of NucleotideCount_Git namespace
